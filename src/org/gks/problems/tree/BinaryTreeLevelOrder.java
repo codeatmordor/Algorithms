7ba9f0a6c26ed2b65af113325ebbb5ecd9e8e570
Gaurav Singh2 <Gaurav_Singh2@symantec.com>
2019-08-22 18:11:42 +0530

    Graph problems

---
+++ src/org/gks/problems/tree/BinaryTreeLevelOrder.java
@@ -0,0 +1,169 @@
+/** SYMANTEC: Copyright 2019 Symantec Corporation. All rights reserved.
+ * THIS SOFTWARE CONTAINS CONFIDENTIAL INFORMATION AND TRADE SECRETS OF
+ * SYMANTEC CORPORATION.USE, DISCLOSURE OR REPRODUCTION IS PROHIBITED
+ * WITHOUT THE PRIOR EXPRESS WRITTEN PERMISSION OF SYMANTEC CORPORATION.
+ * The Licensed Software and Documentation are deemed to be commercial
+ * computer software as defined in FAR 12.212 and subject to restricted
+ * rights as defined in FAR Section 52.227-19 "Commercial Computer Software
+ * - Restricted Rights" and DFARS 227.7202, "Rights in Commercial Computer
+ * Software or Commercial Computer Software Documentation", as applicable,
+ * and any successor regulations.  Any use, modification, reproduction
+ * release, performance, display or disclosure of the Licensed Software
+ * and Documentation by the U.S. Government shall be solely in accordance
+ * with the terms of this Agreement.
+ */
+/********************************************************************
+ * File Name:    BinaryTreeLevelOrder.java
+ *
+ * Date Created: Aug 22, 2019
+ *
+ * ------------------------------------------------------------------
+ * Copyright (C) 2019 Symantec Ltd. All Rights Reserved.
+ *
+ *******************************************************************/
+
+// PACKAGE/IMPORTS --------------------------------------------------
+package org.gks.problems.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author Gaurav_Singh3
+ *
+ */
+class TreeNode {
+    Integer val;
+    TreeNode left;
+    TreeNode right;
+
+    /**
+     * @param val
+     */
+    public TreeNode(int val) {
+        super();
+        this.val = val;
+    }
+}
+
+public class BinaryTreeLevelOrder {
+
+    public static List<List<Integer>> levelOrder(TreeNode r) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        q.add(r);
+
+        int level = 1;
+        while (!q.isEmpty()) {
+            List<Integer> l = new ArrayList<>();
+            int levelSize = q.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = q.poll();
+                l.add(node.val);
+                if (node.left != null)
+                    q.offer(node.left);
+                if (node.right != null)
+                    q.offer(node.right);
+            }
+            res.add(l);
+        }
+
+        return res;
+
+    }
+
+    public static List<List<Integer>> zigzag(TreeNode r) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> res = new LinkedList<>();
+        q.add(r);
+
+        int level = 1;
+        boolean leftToRight = true;
+        while (!q.isEmpty()) {
+            List<Integer> l = new ArrayList<>();
+            int levelSize = q.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = q.poll();
+                if (leftToRight)
+                    l.add(node.val);
+                else
+                    l.add(0, node.val);
+                if (node.left != null)
+                    q.offer(node.left);
+                if (node.right != null)
+                    q.offer(node.right);
+            }
+            res.add(l);
+            leftToRight = !leftToRight;
+        }
+
+        return res;
+
+    }
+
+    public static List<List<Integer>> reverselevelOrder(TreeNode r) {
+        Queue<TreeNode> q = new LinkedList<>();
+        List<List<Integer>> res = new LinkedList<>();
+        q.add(r);
+
+        int level = 1;
+        while (!q.isEmpty()) {
+            List<Integer> l = new ArrayList<>();
+            int levelSize = q.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = q.poll();
+                l.add(node.val);
+                if (node.left != null)
+                    q.offer(node.left);
+                if (node.right != null)
+                    q.offer(node.right);
+            }
+            res.add(0, l);
+        }
+
+        return res;
+
+    }
+
+    BinaryTreeLevelOrder() {
+
+    }
+
+    TreeNode r;
+
+    public static void main(String[] args) {
+
+        BinaryTreeLevelOrder b = new BinaryTreeLevelOrder();
+        b.r = new TreeNode(5);
+        b.r.left = new TreeNode(6);
+        b.r.right = new TreeNode(7);
+        b.r.left.left = new TreeNode(8);
+        b.r.left.right = new TreeNode(9);
+        b.r.right.left = new TreeNode(10);
+        b.r.right.right = new TreeNode(11);
+        b.levelOrder(b.r).stream().forEach(l -> {
+            l.stream().forEach(s -> System.out.print(s + " "));
+            System.out.println();
+        });
+
+        System.out.print(System.lineSeparator());
+        System.out.print(System.lineSeparator());
+
+        b.reverselevelOrder(b.r).stream().forEach(l -> {
+            l.stream().forEach(s -> System.out.print(s + " "));
+            System.out.println();
+        });
+
+        System.out.print(System.lineSeparator());
+        System.out.print(System.lineSeparator());
+
+        b.zigzag(b.r).stream().forEach(l -> {
+            l.stream().forEach(s -> System.out.print(s + " "));
+            System.out.println();
+        });
+
+    }
+
+}
---
+++ src/org/gks/problems/tree/MaxCableLengthDFS.java
@@ -0,0 +1,59 @@
+/** SYMANTEC: Copyright 2019 Symantec Corporation. All rights reserved.
+ * THIS SOFTWARE CONTAINS CONFIDENTIAL INFORMATION AND TRADE SECRETS OF
+ * SYMANTEC CORPORATION.USE, DISCLOSURE OR REPRODUCTION IS PROHIBITED
+ * WITHOUT THE PRIOR EXPRESS WRITTEN PERMISSION OF SYMANTEC CORPORATION.
+ * The Licensed Software and Documentation are deemed to be commercial
+ * computer software as defined in FAR 12.212 and subject to restricted
+ * rights as defined in FAR Section 52.227-19 "Commercial Computer Software
+ * - Restricted Rights" and DFARS 227.7202, "Rights in Commercial Computer
+ * Software or Commercial Computer Software Documentation", as applicable,
+ * and any successor regulations.  Any use, modification, reproduction
+ * release, performance, display or disclosure of the Licensed Software
+ * and Documentation by the U.S. Government shall be solely in accordance
+ * with the terms of this Agreement.
+ */
+/********************************************************************
+ * File Name:    MaxCableLengthDFS.java
+ *
+ * Date Created: Aug 22, 2019
+ *
+ * ------------------------------------------------------------------
+ * Copyright (C) 2019 Symantec Ltd. All Rights Reserved.
+ *
+ *******************************************************************/
+
+// PACKAGE/IMPORTS --------------------------------------------------
+package org.gks.problems.tree;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Gaurav_Singh3
+ *
+ */
+public class MaxCableLengthDFS {
+
+    private static class Edge {
+        private Integer node;
+        private Integer weight;
+
+        /**
+         * @param node
+         * @param weight
+         */
+        public Edge(Integer node, Integer weight) {
+            super();
+            this.node = node;
+            this.weight = weight;
+        }
+
+    }
+
+    public static List<Edge> graph[7];
+
+    public static void main(String[] args) {
+
+    }
+
+}
