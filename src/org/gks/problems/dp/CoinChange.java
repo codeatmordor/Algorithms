4f0176ac95181c369c475d8ee4da98f5cab7e2a2
codeatmordor <gaurav.bhu3790@gmail.com>
2019-10-05 17:40:53 +0530

    Coin change

--- src/org/gks/problems/dp/CoinChange.java
+++ src/org/gks/problems/dp/CoinChange.java
@@ -3,9 +3,17 @@
 // PACKAGE/IMPORTS --------------------------------------------------
 package org.gks.problems.dp;
 
+class Calls {
+  public long count;
+
+  Calls() {
+    count = 0;
+  }
+}
+
 
 public class CoinChange {
-  public static int minCoinRecur(int[] coins, int val) {
+  public static int minCoinRecur(int[] coins, int val, Calls calls) {
     // System.out.println(val);
     if (val == 0)
       return 0;
@@ -13,7 +21,8 @@ public class CoinChange {
 
     for (int c : coins) {
       if (val - c >= 0) {
-        int i = minCoinRecur(coins, val - c);
+        calls.count++;
+        int i = minCoinRecur(coins, val - c, calls);
         if (min > i + 1)
           min = i + 1;
       }
@@ -21,15 +30,17 @@ public class CoinChange {
     return min;
   }
 
-  public static int minCoinDP(int[] coins, int val) {
-    int[] cache = new int[val];
-    for (int i = 1; i < val; i++) {
+  public static int minCoinDP(int[] coins, int val, Calls calls) {
+    int[] cache = new int[val + 1];
+    for (int i = 1; i <= val; i++) {
       cache[i] = -1;
     }
-    return minCoin(coins, val, cache);
+
+    return minCoin(coins, val, cache, calls);
   }
 
-  private static int minCoin(int[] coins, int val, int[] cache) {
+
+  private static int minCoin(int[] coins, int val, int[] cache, Calls calls) {
     // System.out.println(val);
     if (val == 0)
       return 0;
@@ -38,10 +49,13 @@ public class CoinChange {
     for (int c : coins) {
       if (val - c >= 0) {
         int i;
-        if (cache[val - c] >= 0)
+        if (cache[val - c] >= 0) {
           i = cache[val - c];
-        else {
-          i = minCoin(coins, val - c, cache);
+        } else {
+          calls.count++;
+          i = minCoin(coins, val - c, cache, calls);
+          // Update Cache
+          cache[val - c] = i;
         }
         if (min > i + 1)
           min = i + 1;
@@ -54,9 +68,14 @@ public class CoinChange {
 
   public static void main(String[] args) {
     int[] coins = new int[] {10, 20, 25, 50};
+    int val = 400;
+    Calls calls1 = new Calls();
+    System.out.println(minCoinRecur(coins, val, calls1));
+    System.out.println(calls1.count);
+    calls1.count = 0;
+    System.out.println(minCoinDP(coins, val, calls1));
+    System.out.println(calls1.count);
 
-    System.out.println(minCoinRecur(coins, 120));
-    System.out.println(minCoinDP(coins, 120));
   }
 }
 
