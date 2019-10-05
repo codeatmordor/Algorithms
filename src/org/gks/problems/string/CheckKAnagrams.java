e11985809df05cd4ec337335ca378f70e0810d39
codeatmordor <gaurav.bhu3790@gmail.com>
2019-10-05 18:06:06 +0530

    Are Two String kAnagram of Each Other

--- src/org/gks/problems/string/CheckKAnagrams.java
+++ src/org/gks/problems/string/CheckKAnagrams.java
@@ -2,30 +2,47 @@
 // PACKAGE/IMPORTS --------------------------------------------------
 package org.gks.problems.string;
 
-//https://www.geeksforgeeks.org/check-two-strings-k-anagrams-not/
+// https://www.geeksforgeeks.org/check-two-strings-k-anagrams-not/
 public class CheckKAnagrams {
+  static final int MAX_CHAR = 26;
 
-    static boolean arekAnagrams(String str1, String str2, int k) {
-        int[] char1Count = new int[26];
-        int[] char2Count = new int[26];
+  static boolean arekAnagrams(String str1, String str2, int k) {
+    // If both strings are not of equal
+    // length then return false
+    int n = str1.length();
+    if (str2.length() != n)
+      return false;
 
-        if(str1.length()!=str2.length())
-            return false;
+    int[] count1 = new int[MAX_CHAR];
+    int[] count2 = new int[MAX_CHAR];
+    int count = 0;
 
-        for(int i=0;i<str1.length();i++) {
-            char
-        }
+    // Store the occurrence of all characters
+    // in a hash_array
+    for (int i = 0; i < n; i++)
+      count1[str1.charAt(i) - 'a']++;
+    for (int i = 0; i < n; i++)
+      count2[str2.charAt(i) - 'a']++;
 
-    }
+    // Count number of characters that are
+    // different in both strings
+    for (int i = 0; i < MAX_CHAR; i++)
+      if (count1[i] > count2[i])
+        count = count + Math.abs(count1[i] - count2[i]);
 
-    public static void main(String[] args) {
-        String str1 = "anagram";
-        String str2 = "grammar";
-        int k = 2;
-        if (arekAnagrams(str1, str2, k))
-            System.out.println("Yes");
-        else
-            System.out.println("No");
-    }
+    // Return true if count is less than or
+    // equal to k
+    return (count <= k);
+  }
+
+  public static void main(String[] args) {
+    String str1 = "anagram";
+    String str2 = "grammar";
+    int k = 2;
+    if (arekAnagrams(str1, str2, k))
+      System.out.println("Yes");
+    else
+      System.out.println("No");
+  }
 
 }
