72479f12e175434fc6d71c1c2f201c022e6f3d67
codeatmordor <gaurav.bhu3790@gmail.com>
2019-10-07 09:32:26 +0530

    All sequence of BST

--- src/org/gks/problems/tree/BinarySearchTree.java
+++ src/org/gks/problems/tree/BinarySearchTree.java
@@ -2,6 +2,10 @@
 // PACKAGE/IMPORTS --------------------------------------------------
 package org.gks.problems.tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class BinarySearchTree {
 
   static Node root;
@@ -116,6 +120,64 @@ public class BinarySearchTree {
     return node;
   }
 
+  public static List<LinkedList<Integer>> allSequences(Node node) {
+    List<LinkedList<Integer>> result = new ArrayList<>();
+    if (node == null) {
+      result.add(new LinkedList<>());
+      return result;
+    }
+
+    LinkedList<Integer> prefix = new LinkedList<>();
+    prefix.add(node.data);
+
+    /* Recurse on left and right subtrees. */
+    List<LinkedList<Integer>> leftSeq = allSequences(node.left);
+    System.out.println("For Left Node - " + node.left);
+    leftSeq.stream().forEach(l -> System.out.println(l));
+    List<LinkedList<Integer>> rightSeq = allSequences(node.right);
+    System.out.println("For right Node - " + node.right);
+    rightSeq.stream().forEach(l -> System.out.println(l));
+
+    /* Weave together each list from the left and right sides. */
+    for (LinkedList<Integer> left : leftSeq) {
+      for (LinkedList<Integer> right : rightSeq) {
+        ArrayList<LinkedList<Integer>> weaved = new ArrayList<>();
+        weaveLists(left, right, weaved, prefix);
+        result.addAll(weaved);
+      }
+    }
+    return result;
+  }
+
+  private static void weaveLists(LinkedList<Integer> first, LinkedList<Integer> second,
+      List<LinkedList<Integer>> results, LinkedList<Integer> prefix) {
+    /* One list is empty. Add remainder to [a cloned] prefix and store result. */
+    if (first.size() == 0 || second.size() == 0) {
+      LinkedList<Integer> result = (LinkedList<Integer>) prefix.clone();
+      result.addAll(first);
+      result.addAll(second);
+      results.add(result);
+      return;
+    }
+
+    /*
+     * Recurse with head of first added to the prefix. Removing the head will damage first, so we'll
+     * need to put it back where we found it afterwards.
+     */
+    int headFirst = first.removeFirst();
+    prefix.addLast(headFirst);
+    weaveLists(first, second, results, prefix);
+    prefix.removeLast();
+    first.addFirst(headFirst);
+
+    /* Do the same thing with second, damaging and then restoring the list. */
+    int headSecond = second.removeFirst();
+    prefix.addLast(headSecond);
+    weaveLists(first, second, results, prefix);
+    prefix.removeLast();
+    second.addFirst(headSecond);
+  }
+
   public static void main(String[] args) {
     BinarySearchTree tree = new BinarySearchTree();
 
@@ -149,27 +211,28 @@ public class BinarySearchTree {
     tree.inorder();
 
     BinarySearchTree t1 = new BinarySearchTree();
-    t1.root = new Node(20);
-    t1.root.left = new Node(8);
-    t1.root.right = new Node(22);
-    t1.root.left.left = new Node(4);
-    t1.root.left.right = new Node(12);
-    t1.root.left.right.left = new Node(10);
-    t1.root.left.right.right = new Node(14);
+    t1.root = new Node(2);
+    t1.root.left = new Node(1);
+    t1.root.right = new Node(3);
+    /*
+     * t1.root.left.left = new Node(4); t1.root.left.right = new Node(12); t1.root.left.right.left =
+     * new Node(10); t1.root.left.right.right = new Node(14);
+     */
+    /*
+     * int n1 = 10, n2 = 14; Node t = t1.lca(t1.root, n1, n2); System.out.println("LCA of " + n1 +
+     * " and " + n2 + " is " + t.data);
+     *
+     * n1 = 14; n2 = 8; t = t1.lca(t1.root, n1, n2); System.out.println("LCA of " + n1 + " and " +
+     * n2 + " is " + t.data);
+     *
+     * n1 = 10; n2 = 22; t = t1.lca(t1.root, n1, n2); System.out.println("LCA of " + n1 + " and " +
+     * n2 + " is " + t.data);
+     */
+    List<LinkedList<Integer>> res = allSequences(t1.root);
+    System.out.println("========== ALL BST SEQUENCES ============");
+    System.out.println();
+    res.stream().forEach(l -> System.out.println(l));
 
-    int n1 = 10, n2 = 14;
-    Node t = t1.lca(t1.root, n1, n2);
-    System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data);
-
-    n1 = 14;
-    n2 = 8;
-    t = t1.lca(t1.root, n1, n2);
-    System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data);
-
-    n1 = 10;
-    n2 = 22;
-    t = t1.lca(t1.root, n1, n2);
-    System.out.println("LCA of " + n1 + " and " + n2 + " is " + t.data);
   }
 
 }
