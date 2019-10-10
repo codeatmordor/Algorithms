9c7e6567f56d3f9b28b4638e2f72d50b1b67c570
codeatmordor <gaurav.bhu3790@gmail.com>
2019-10-10 10:57:15 +0530

    Code refactoring

--- src/org/gks/problems/dp/SubsetSum.java
+++ src/org/gks/problems/dp/SubsetSum.java
@@ -4,56 +4,62 @@ package org.gks.problems.dp;
 
 public class SubsetSum {
 
-    static boolean isSubsetSum(int[] set, int n, int sum) {
-        if (sum == 0)
-            return true;
-        if (n == 0 && sum != 0)
-            return false;
+  static boolean isSubsetSum(int[] set, int n, int sum) {
+    if (sum == 0)
+      return true;
+    if (n == 0 && sum != 0)
+      return false;
 
-        if (set[n - 1] > sum)
-            return isSubsetSum(set, n - 1, sum);
+    // If last element is greater than
+    // sum, then ignore it
+    if (set[n - 1] > sum)
+      return isSubsetSum(set, n - 1, sum);
 
-        return isSubsetSum(set, n - 1, sum) || isSubsetSum(set, n - 1, sum - set[n - 1]);
+    /*
+     * else, check if sum can be obtained by any of the following (a) including the last element (b)
+     * excluding the last element
+     */
+    return isSubsetSum(set, n - 1, sum) || isSubsetSum(set, n - 1, sum - set[n - 1]);
 
+  }
+
+  static boolean isSubsetSumDP(int[] set, int n, int sum) {
+    boolean[][] subset = new boolean[sum + 1][n + 1];
+
+    for (int i = 0; i <= n; i++) {
+      subset[0][i] = true;
     }
 
-    static boolean isSubsetSumDP(int[] set, int n, int sum) {
-        boolean[][] subset = new boolean[sum + 1][n + 1];
-
-        for (int i = 0; i <= n; i++) {
-            subset[0][i] = true;
-        }
-
-        for (int i = 1; i <= sum; i++) {
-            subset[i][0] = false;
-        }
-
-        for (int i = 1; i <= sum; i++) {
-            for (int j = 1; j <= n; j++) {
-                subset[i][j] = subset[i][j - 1];
-                if (i >= set[j - 1])
-                    subset[i][j] = subset[i][j] || subset[i - set[j - 1]][j - 1];
-            }
-        }
-
-        for (int i = 0; i <= sum; i++) {
-            for (int j = 0; j <= n; j++)
-                System.out.print(subset[i][j] + " ");
-            System.out.println();
-        }
-
-        return subset[sum][n];
-
+    for (int i = 1; i <= sum; i++) {
+      subset[i][0] = false;
     }
 
-    public static void main(String[] args) {
-        int set[] = { 3, 34, 4, 12, 5, 2 };
-        int sum = 9;
-        int n = set.length;
-        if (isSubsetSumDP(set, n, sum) == true)
-            System.out.println("Found a subset" + " with given sum");
-        else
-            System.out.println("No subset with" + " given sum");
+    for (int i = 1; i <= sum; i++) {
+      for (int j = 1; j <= n; j++) {
+        subset[i][j] = subset[i][j - 1];
+        if (i >= set[j - 1])
+          subset[i][j] = subset[i][j] || subset[i - set[j - 1]][j - 1];
+      }
     }
+
+    for (int i = 0; i <= sum; i++) {
+      for (int j = 0; j <= n; j++)
+        System.out.print(subset[i][j] + " ");
+      System.out.println();
+    }
+
+    return subset[sum][n];
+
+  }
+
+  public static void main(String[] args) {
+    int set[] = {3, 34, 4, 12, 5, 2};
+    int sum = 9;
+    int n = set.length;
+    if (isSubsetSumDP(set, n, sum) == true)
+      System.out.println("Found a subset" + " with given sum");
+    else
+      System.out.println("No subset with" + " given sum");
+  }
 
 }
--- src/org/gks/problems/linkedlist/LinkList.java
+++ src/org/gks/problems/linkedlist/LinkList.java
@@ -3,143 +3,114 @@ package org.gks.problems.linkedlist;
 import java.util.ArrayList;
 import java.util.List;
 
-/** SYMANTEC: Copyright 2019 Symantec Corporation. All rights reserved.
- * THIS SOFTWARE CONTAINS CONFIDENTIAL INFORMATION AND TRADE SECRETS OF
- * SYMANTEC CORPORATION.USE, DISCLOSURE OR REPRODUCTION IS PROHIBITED
- * WITHOUT THE PRIOR EXPRESS WRITTEN PERMISSION OF SYMANTEC CORPORATION.
- * The Licensed Software and Documentation are deemed to be commercial
- * computer software as defined in FAR 12.212 and subject to restricted
- * rights as defined in FAR Section 52.227-19 "Commercial Computer Software
- * - Restricted Rights" and DFARS 227.7202, "Rights in Commercial Computer
- * Software or Commercial Computer Software Documentation", as applicable,
- * and any successor regulations.  Any use, modification, reproduction
- * release, performance, display or disclosure of the Licensed Software
- * and Documentation by the U.S. Government shall be solely in accordance
- * with the terms of this Agreement.
- */
-/********************************************************************
- * File Name:    LinkList.java
- *
- * Date Created: Aug 18, 2019
- *
- * ------------------------------------------------------------------
- * Copyright (C) 2019 Symantec Ltd. All Rights Reserved.
- *
- *******************************************************************/
 
-// PACKAGE/IMPORTS --------------------------------------------------
-
-/**
- * @author Gaurav_Singh3
- *
- */
 public class LinkList {
 
-    private class Node {
-        String value;
-        Node next;
+  private class Node {
+    String value;
+    Node next;
 
-        Node(String value) {
-            this.value = value;
-            this.next = null;
-        }
+    Node(String value) {
+      this.value = value;
+      this.next = null;
+    }
+  }
+
+  private Node start;
+  private int length;
+
+  /**
+   * @param start
+   * @param length
+   */
+  public LinkList(String value) {
+    super();
+    this.start = new Node(value);
+    start.next = null;
+    this.length = 1;
+  }
+
+  public void add(String value) {
+    Node curr = start;
+    while (curr.next != null) {
+      curr = curr.next;
+    }
+    Node n = new Node(value);
+    n.next = null;
+    curr.next = n;
+    length++;
+  }
+
+  public String getNthToLastNode(int n) {
+    if (n > length)
+      throw new IllegalArgumentException();
+    int frombegin = (length - n) + 1;
+    int count = 1;
+    Node curr = start;
+    while (count < frombegin) {
+      curr = curr.next;
+      count++;
+    }
+    return curr.value;
+  }
+
+  public List<Node> split() {
+    Node list = this.start;
+    // runner will be the fast moving pointer
+    Node runner = list.next;
+    List<Node> toReturn = new ArrayList<>();
+
+    while (runner != null) {
+      runner = runner.next;
+      if (runner == null)
+        break;
+      runner = runner.next;
+      list = list.next;
+    }
+    toReturn.add(list.next);
+    list.next = null;
+    toReturn.add(this.start);
+    return toReturn;
+  }
+
+  public void displayMiddle() {
+    Node list = this.start;
+    Node fast = list;
+
+    while (fast != null && fast.next != null) {
+      if (fast == null) {
+        System.out.println(list.toString());
+        break;
+      }
+
+      list = list.next;
+      if (fast.next.next != null)
+        fast = fast.next.next;
     }
 
-    private Node start;
-    private int length;
+  }
 
-    /**
-     * @param start
-     * @param length
-     */
-    public LinkList(String value) {
-        super();
-        this.start = new Node(value);
-        start.next = null;
-        this.length = 1;
+  public void reverse() {
+    Node n = this.start;
+    Node prev = null;
+    Node temp = null;
+    while (n != null) {
+      temp = n.next;
+      n.next = prev;
+      prev = n;
+      n = temp;
+    }
+    this.start = prev;
+
+  }
+
+  public void display() {
+    Node n = this.start;
+
+    while (n != null) {
+      System.out.println(n.value);
+      n = n.next;
     }
 
-    public void add(String value) {
-        Node curr = start;
-        while (curr.next != null) {
-            curr = curr.next;
-        }
-        Node n = new Node(value);
-        n.next = null;
-        curr.next = n;
-        length++;
-    }
-
-    public String getNthToLastNode(int n) {
-        if (n > length)
-            throw new IllegalArgumentException();
-        int frombegin = (length - n) + 1;
-        int count = 1;
-        Node curr = start;
-        while (count < frombegin) {
-            curr = curr.next;
-            count++;
-        }
-        return curr.value;
-    }
-
-    public List<Node> split() {
-        Node list = this.start;
-        // runner will be the fast moving pointer
-        Node runner = list.next;
-        List<Node> toReturn = new ArrayList<>();
-
-        while (runner != null) {
-            runner = runner.next;
-            if (runner == null)
-                break;
-            runner = runner.next;
-            list = list.next;
-        }
-        toReturn.add(list.next);
-        list.next = null;
-        toReturn.add(this.start);
-        return toReturn;
-    }
-
-    public void displayMiddle() {
-        Node list = this.start;
-        Node fast = list;
-
-        while (fast != null && fast.next != null) {
-            if (fast == null) {
-                System.out.println(list.toString());
-                break;
-            }
-
-            list = list.next;
-            if (fast.next.next != null)
-                fast = fast.next.next;
-        }
-
-    }
-
-    public void reverse() {
-        Node n = this.start;
-        Node prev = null;
-        Node temp = null;
-        while (n != null) {
-            temp = n.next;
-            n.next = prev;
-            prev = n;
-            n = temp;
-        }
-        this.start = prev;
-
-    }
-
-    public void display() {
-        Node n = this.start;
-
-        while (n != null) {
-            System.out.println(n.value);
-            n = n.next;
-        }
-
-    }
+  }
 }
