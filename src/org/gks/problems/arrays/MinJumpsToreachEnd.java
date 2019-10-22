a1a4f4630d13e1de10497a50087f2bf35b982c1d
codeatmordor <gaurav.bhu3790@gmail.com>
2019-10-22 23:07:00 +0530

    Min jumps

---
+++ src/org/gks/problems/arrays/MinJumpsToreachEnd.java
@@ -0,0 +1,36 @@
+package org.gks.problems.arrays;
+
+// https://www.geeksforgeeks.org/minimum-number-of-jumps-to-reach-end-of-a-given-array/
+public class MinJumpsToreachEnd {
+
+
+  public static int minjumps(int[] input) {
+
+    int[] jumps = new int[input.length];
+    if (input[0] == 0)
+      return Integer.MAX_VALUE;
+
+    jumps[0] = 0;
+
+    // Find the minimum number of jumps to reach arr[i]
+    // from arr[0], and assign this value to jumps[i]
+    for (int i = 1; i < input.length; i++) {
+      jumps[i] = Integer.MAX_VALUE;
+
+      for (int j = 0; j < i; j++) {
+        if (i <= j + input[j] && jumps[j] != Integer.MAX_VALUE) {
+          jumps[i] = Math.min(jumps[i], jumps[j] + 1);
+          break;
+        }
+      }
+    }
+    return jumps[input.length - 1];
+
+  }
+
+  public static void main(String[] args) {
+    int[] input = {1, 3, 6, 1, 0, 9};
+    System.out.println(minjumps(input));
+  }
+}
+
